69042bc7653b14e5ca8301ef5a0c77b2ce367573
rwoormip <rwoormip510@gmail.com>
2023-02-05 18:32:05 +0800

    Initialize Project

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const exphbs = require('express-handlebars')
+const restaurantList = require('./restaurant.json')
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.render('index', { restaurants: restaurantList.results })
+})
+
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  // console.log(req.params.restaurant_id)
+  const restaurant = restaurantList.results.find(
+    restaurant => restaurant.id.toString() === req.params.restaurant_id
+  )
+  res.render('show', { restaurant: restaurant })
+})
+
+app.listen(port, () => {
+  console.log(`Express is Listening on localhost:${port}`)
+})
