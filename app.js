88f0ed9058d332616ca71012b08ca293e6bda9cc
rwoormip <rwoormip510@gmail.com>
2023-02-25 11:22:43 +0800

    feat: add delete restaurant function

--- app.js
+++ app.js
@@ -95,6 +95,14 @@ app.post('/restaurants/:id/edit', (req, res) => {
     .catch(error => console.log(error))
 })
 
+app.post('/restaurants/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .then(restaurant => restaurant.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
 app.listen(port, () => {
   console.log(`App is running on http://localhost:${port}`)
 })
