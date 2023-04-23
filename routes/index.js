19c54805b9cbbe763280fb1e9310866ab1d6b32a
rwoormip <rwoormip510@gmail.com>
2023-04-23 13:10:38 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,12 @@ const router = express.Router()
 
 const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
+const users = require('./modules/users')
 const search = require('./modules/search')
 
-router.use('/', home)
 router.use('/restaurants', restaurants)
+router.use('/users', users)
 router.use('/search', search)
+router.use('/', home)
 
 module.exports = router
