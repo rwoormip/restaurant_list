f54585c239d2a5e2def638ae340266c03a938091
rwoormip <rwoormip510@gmail.com>
2023-04-23 13:27:53 +0800

    Revert "feat: add user login page"
    This reverts commit 3026d286534ac2a205ee8d45f425eca0532a1f68.

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -5,12 +5,4 @@ router.get('/login', (req, res) => {
   res.render('login')
 })
 
-router.post('/login', (req, res) => {
-
-})
-
-router.get('/register', (req, res) => {
-  res.render('register')
-})
-
 module.exports = router
